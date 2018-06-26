595d7231d4db6e5f5ac4e5d5362b7eb0ed004bd9
Duisters <duistersklw@m0028184.local>
2018-06-25 23:33:10 -0700

    Py practice

---
+++ Python/Learnpy.py
@@ -0,0 +1,16 @@
+height = 1.90
+weight = 75
+bmi = weight/height**2
+print(bmi)
+
+# printing strings (paste). Use the + for strings
+print('look a string ' + str(bmi))
+
+# Lists of lists
+fam = [ ["Kev", 1.90], ["John", 1.81] , ["Emily", 1.65]  ]
+
+# indexing!! Start at 0; Ends at -1. A range is from up to (not included!)
+fam[1]
+fam[0:2]
+
+

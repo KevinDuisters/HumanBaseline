7a93419782fcf61d6c0a18100fb456ee95c100ec
Kevin Duisters <kevin.duisters@gmail.com>
2019-01-22 11:59:57 +0100

    final update before submit

--- 181218 Visualisations.R
+++ 181218 Visualisations.R
@@ -14,7 +14,10 @@
 # load packages
 library(readxl)
 library(ggplot2)
+library(gridExtra) # grid.arrange
+library(ggpubr) # ggarrange
 library(gplots) # heatmap
+library(ggrepel) # labels in geom point
 library(RColorBrewer)
 
 
@@ -33,16 +36,15 @@ ST1[order(ST1$sigma_time,decreasing=T)[1:10],c(1,2,18,19,20)] # for heatmap Fig 
 # Figures Variance and subVariance (supplemental fig) 
 
 
-#sub <- F;pdf("Figures/Variance.pdf",width=9)
-#sub<-T;k = 1;pdf("Figures/subVariance1.pdf",width=9)
-sub<-T;k = 2;pdf("Figures/subVariance2.pdf",width=9)
-#sub<-T;k = 3;pdf("Figures/subVariance3.pdf",width=9)
+sub <- F;K=1;pdf("Figures/Variance.pdf",width=8.7);plotlist <- vector("list",K)
+#sub <- T; K = 3;pdf("Figures/Variancepanel.pdf",width=8.7);plotlist <- vector("list",K)
 
 
+for(k in 1:K){
 # sort to make bubbles look nice on top of each other
 panelnames <- c("SomaLogic","Metabolon","BMFL")
 mysort <- c(which(ST1$panel==(panelnames[1])),which(ST1$panel==(panelnames[2])),which(ST1$panel==(panelnames[3])))
-mylabs <- c("Plasma Proteins","Plasma Metabolon","Urine Amines")
+mylabs <- c("Plasma Proteins","Plasma Metabolites","Urine Amines")
 if(sub==F){dynamictitle<-""}else{dynamictitle<-paste0(mylabs[k]," [",sum(ST1$panel==(panelnames[k])),"]")} # plot title
 mycols <- c("lightblue","white","navy") # rgb Leiden Univ color #rgb(0,17,88,maxColorValue = 255)
 Platform <- numeric(length(ST1$panel))
@@ -59,8 +61,8 @@ V <- data.frame(sigmab=mysb,sigmaeps=ST1$sigma_eps[mysort],sigmatime=ST1$sigma_t
 
 gout <- ggplot(V, aes(x=sigmaeps, y=sigmab, size=(sigmatime))) +
           labs(title=dynamictitle,subtitle="")+
-          xlim(0,2.2)+ylim(0,2.2)+
-            geom_point(shape=21,colour="black",alpha=0.8,aes(fill=platformV))+
+          xlim(0,2.4)+ylim(0,2.4)+
+            geom_point(shape=21,colour="black",alpha=0.8,aes(fill=platformV),show.legend = (sub==F))+
             scale_fill_manual(name="Platform",values=mycols) +
           scale_size_continuous(range=c(0, 13)) +    # range can be used to scale the bubbles
           ylab(expression(paste("Between ",(sigma[b])))) +
@@ -68,15 +70,25 @@ gout <- ggplot(V, aes(x=sigmaeps, y=sigmab, size=(sigmatime))) +
           labs( size = expression(paste("Time ",(sigma[tau])))) +
           theme_bw() +
           theme(
-            plot.title = element_text(size=15,hjust = 0.5,face="bold"),
-            text = element_text(size=11),
+            plot.title = element_text(size=12,hjust = 0.5,face="bold"),
+            text = element_text(size=10),
             legend.position = c(.85, .1),
             legend.justification = c("center", "bottom"),
             panel.border = element_blank()
           ) +
-          guides(fill = guide_legend(override.aes = list(shape=21,size=12)))
-print(gout)
+          guides(fill = guide_legend(override.aes = list(shape=21,size=12)))+coord_fixed()
+plotlist[[k]] <- gout
+#print(gout)
+}
+
+if(sub==T){grid.arrange(arrangeGrob(plotlist[[1]],plotlist[[2]],plotlist[[3]],ncol=2))}else{print(plotlist[[1]])}
+             
+# fix a nice legend later before submission, for now switch it off in SI subplots
+
+
+
 dev.off()
+
 #-----------------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------------
 # Fig 2: Profiles
@@ -85,7 +97,7 @@ dev.off()
 numtimes  <- c(-2,-0.5,0.5,1.5,3,4,4.5,6,8,9.5,11,12)
 shortnames <- ST1$compound
 for(j in 1:nrow(ST1)){
-  if(nchar(ST1$compound[j])>20){shortnames[j] <- paste0(substr(ST1$compound[j],1,20),"...")}
+  if(nchar(ST1$compound[j])>50){shortnames[j] <- paste0(substr(ST1$compound[j],1,50),"...")}
 }
 plat<-ST1$panel
 for(j in 1:length(plat)){
@@ -104,10 +116,14 @@ alpha <- 0.05
 
 
 sel <- c(206,1941,1817,798) # this is sensitive, look up manually
-dynamic <- paste0("Figures/profiles",1:length(sel),".pdf")
+  
+#dynamic <- paste0("Figures/profiles",1:length(sel),".pdf")
+
+pdf("Figures/profiles.pdf",width=8.7)
+plotlist <- vector("list",length(sel))
 
 for(k in 1:length(sel)){
-pdf(dynamic[k],width=9)
+#pdf(dynamic[k],width=9)
 
   j <- sel[k]
 coefs <- as.numeric(ST1[j,4:17])
@@ -126,14 +142,14 @@ aux <- data.frame(low=low[isna],mid=means[isna],high=high[isna],time=numtimes[is
 pnascol <- c(207,214,235)/256
 
 
-print(ggplot(aux,aes(x=time,y=mid))+
+plotlist[[k]] <- (ggplot(aux,aes(x=time,y=mid))+
   #ylab("normalized relative concentration")+
     ylab("")+
   xlab("Time (hrs) since breakfast")+
   xlim(-2,12)+
   ylim(-5,5)+
-  labs(title=paste0(shortnames[j]," (",plat[j],")"),
-       subtitle=bquote(paste(alpha,"=",.(localpha),", ","Age=",.(agei),", BMI=",.(bmii))))+
+  labs(title=shortnames[j],
+       subtitle=bquote(paste(.(plat[j]),", ",alpha,"=",.(localpha),", ","Age=",.(agei),", BMI=",.(bmii))))+
   geom_line(aes(x=time,y=high),linetype="dashed")+
   geom_line(aes(x=time,y=low),linetype="dashed")+
   geom_ribbon(aes(x=time,ymax=high,ymin=low),fill=rgb(pnascol[1],pnascol[2],pnascol[3]),alpha=0)+
@@ -142,17 +158,18 @@ print(ggplot(aux,aes(x=time,y=mid))+
   geom_point(colour="black",size=5)+
   theme_bw()+
   theme(
-    axis.text=element_text(size=25),
-    axis.title=element_text(size=25),
-    plot.title = element_text(size=30,hjust = 0.5,face="bold"),
-    plot.subtitle = element_text(size=25,hjust = 0.5,face="bold"),
+    axis.text=element_text(size=10),
+    axis.title=element_text(size=10),
+    plot.title = element_text(size=12,hjust = 0.5,face="bold"),
+    plot.subtitle = element_text(size=10,hjust = 0.5,face="bold"),
     panel.border=element_blank()
-  ))
+  )+coord_fixed())
 
 
-dev.off()
+#dev.off()
 }
-
+grid.arrange(arrangeGrob(plotlist[[1]],plotlist[[2]],plotlist[[3]],plotlist[[4]],ncol=2))
+dev.off()
 
 #-----------------------------------------------------------------------------------------------------------
 # Discussion example
@@ -201,7 +218,7 @@ for(i in 1:length(top)){
 set[i,] <-  as.numeric(ST1[top[i],6:17])+23.8*as.numeric(ST1[top[i],4])+23.3*as.numeric(ST1[top[i],5])
 }
 
-labs <- as.data.frame(ST1)[order(ST1$sigma_time,decreasing=T)[1:10],1]
+labs <- as.data.frame(ST1)[top,1]
 
 
 pdf("Figures/heat.pdf")
@@ -219,7 +236,7 @@ for(j in 1:length(hm$col)){
   rect(-0.2+ep,0.4+(j-1)*0.025,-0.175+ep,0.425+(j-1)*0.025,col=hm$col[j],xpd=T)
 }
 
-text(x=-0.22+ep,y=0.52,"row zscore",xpd=T,srt=90,cex=0.9)
+text(x=-0.22+ep,y=0.52,"row score",xpd=T,srt=90,cex=0.9)
 text(x=-0.15+ep,y=0.4,"-2",xpd=T,srt=90,cex=0.9)
 text(x=-0.15+ep,y=0.525,"0",xpd=T,srt=90,cex=0.9)
 text(x=-0.15+ep,y=0.65,"2",xpd=T,srt=90,cex=0.9)
@@ -232,26 +249,28 @@ dev.off()
 #-----------------------------------------------------------------------------------------------------------
 # Figure 5 boxplot VC
 # Repro Kim et al (2014)
-pdf("Figures/relVCbox.pdf",width=20)
+pdf("Figures/relVCbox.pdf",width=8.7)
 panelnames <- c("SomaLogic","Metabolon","BMFL") # C, A, B
-
+mylabs <- c("Plasma Proteins","Plasma Metabolites","Urine Amines")
+cx <- 1.5
 
 pi.tab <- matrix(NA,3,5)
-#par(mfrow=c(1,3))
-par(mfrow=c(1,3),mar=c(6,8,6,2)+0.1)
+#par(mfrow=c(1,3),mar=c(6,8,6,2)+0.1)
+layout(rbind(c(1,2,3),c(4,4,4)),heights=c(0.5,0.5))  # same as p-values Figure 4
 for(k in c(2,3,1)) {
   loc <- (ST1$panel==(panelnames[k]))
   VCk <- data.frame(between=(ST1$sigma_b[loc])^2,time=(ST1$sigma_time[loc])^2,noise=(ST1$sigma_eps[loc])^2)
-  boxplot(VCk/rowSums(VCk),ylim=c(0,1),main="",xaxt="n",cex.axis=3) # relative proportions
-  axis(side=1,at=c(1,2,3),colnames(VCk),cex.axis=3,line=1,tick=F)
-  title(paste0((LETTERS[c(3,1,2)])[k],": ",mylabs[k]," [",sum(ST1$panel==(panelnames[k])),"]"),cex.main=4,line=3)
+  boxplot(VCk/rowSums(VCk),ylim=c(0,1),main="",xaxt="n",cex.axis=cx) # relative proportions
+  axis(side=1,at=c(1,2,3),colnames(VCk),cex.axis=cx,line=1,tick=F)
+  title(paste0((LETTERS[c(3,1,2)])[k],": ",mylabs[k]," [",sum(ST1$panel==(panelnames[k])),"]"),cex.main=cx,line=3)
   pi.tab[k,] <- c(colMeans(VCk/rowSums(VCk)),median((VCk$between)/((VCk$between)+VCk$time+VCk$noise)),median((VCk$between)/(VCk$between+VCk$noise)))
 }
+plot.new()
 dev.off()
 
 
 # Urine / plasma comparison table
-round(100*pi.tab[,c(2,1,3,4,5)],0)
+round(100*pi.tab[c(2,3,1),],0)
 
 
 #-----------------------------------------------------------------------------------------------------------
@@ -264,9 +283,9 @@ subpath <- paths[match(ST1$compound[sel],paths[,1]),2]
 
 
 
-pdf("Figures/famVariance.pdf",width=9)
+pdf("Figures/famVariance.pdf",width=8.7)
 
-sort <- c(1,3,4,5,7,8,2,6,5)
+sort <- c(5,1,3,4,7,8,2,6)
 us <- unique(superpath)[sort]
 mycols <- brewer.pal(length(us),"RdBu")
 
@@ -278,26 +297,46 @@ for(i in 1:length(us)){
 }  
 V <- data.frame(familyV=us,sigmab=Vm[,2],sigmaeps=Vm[,3],sigmatime=Vm[,4])
 
+
+# Vblank: blank data for common scale with Fig 1
+panelnames <- c("SomaLogic","Metabolon","BMFL")
+mysort <- c(which(ST1$panel==(panelnames[1])),which(ST1$panel==(panelnames[2])),which(ST1$panel==(panelnames[3])))
+mylabs <- c("Plasma Proteins","Plasma Metabolites","Urine Amines")
+Platform <- numeric(length(ST1$panel))
+Platform[which(ST1$panel=="SomaLogic")] <- 1
+Platform[which(ST1$panel=="Metabolon")] <- 2
+Platform[which(ST1$panel=="BMFL")] <- 3
+Platform <- Platform[mysort]
+Platform <- factor(Platform,levels=c(1,2,3),labels=mylabs)
+mysb <- ST1$sigma_b[mysort]
+Vblank <- data.frame(sigmab=mysb,sigmaeps=ST1$sigma_eps[mysort],sigmatime=ST1$sigma_time[mysort],name=ST1$compound[mysort],platformV=Platform)
+#
+
+
 gout <- ggplot(V, aes(x=sigmaeps, y=sigmab, size=(sigmatime))) +
   labs(title="",subtitle="")+
   xlim(0,1.0)+ylim(0,1.0)+
+  geom_blank(aes(x=sigmaeps, y=sigmab, size=(sigmatime)),Vblank)+
   geom_point(shape=21,colour="black",alpha=0.8,aes(fill=familyV),show.legend = FALSE)+
-  geom_point(shape=21,colour="black",alpha=0.8)+
+  geom_text_repel(aes(label=familyV),show.legend=FALSE,size=5)+
+  #geom_text(size=4,aes(label=familyV) ,nudge_x = 0.0,nudge_y = 0.0)+
+  geom_point(shape=21,colour="black",alpha=0.8)+ # legend size key
   scale_fill_manual(name="Family",values=mycols) +
   scale_size_continuous(range=c(0, 13)) +    # range can be used to scale the bubbles
-  geom_text(size=4,aes(label=familyV) ,nudge_x = 0.0,nudge_y = 0.0)+
   ylab(expression(paste("Between ",(sigma[b])))) +
   xlab(expression(paste("Noise ",(sigma[epsilon])))) +
   labs( size = expression(paste("Time ",(sigma[tau])))) +
   theme_bw() +
   theme(
-    plot.title = element_text(size=15,hjust = 0.5,face="bold"),
-    text = element_text(size=11),
+    plot.title = element_text(size=12,hjust = 0.5,face="bold"),
+    text = element_text(size=10),
     legend.position = c(.15, .1),
     legend.justification = c("center", "bottom"),
     panel.border = element_blank()
   ) +
-  guides(fill = guide_legend(override.aes = list(shape=21,size=12)))
+  guides(fill = guide_legend(override.aes = list(shape=21,size=12)))+coord_fixed()
+
+  
 print(gout)
 
 
@@ -308,20 +347,20 @@ dev.off()
 #-----------------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------------
 # Repro Maitre et al (2017)
-# ICC on urine
-# TO BE UPDATED
+# ICC(t) on urine
 #-----------------------------------------------------------------------------------------------------------
-#pdf("Figures/ICC.pdf")
-icccols <- c("black",cols[3],cols[5],cols[6])
-swmat<-sqrt(slist[[3]]^2-s2blist[[3]])
-ICC <- sqrt(s2blist[[3]])/(sqrt(s2blist[[3]]) + swmat)
+pdf("Figures/ICC.pdf")
+icccols <- c("black",brewer.pal(6,"Blues")[c(2,4,5)])
+B <- (ST1$panel == "BMFL") & (ST1$sample == "urine")
+ICC <- ((ST1$sigma_b[B])^2)/(((ST1$sigma_b[B])^2) + ((ST1[B,c(21,26,29,32)])^2))
+rownames(ICC) <- ST1$compound[B]
 srtd <- order(rowMeans(ICC))
 
 par(xpd=F)
 par(mar=c(5,13.5,4,2)+0.1)
 plot(0,xlim=c(0,1),ylim=c(1,63),type="n",ylab="",xlab="",yaxt="n",xaxt="n",bty="n")
-axis(side=1,seq(0,1,0.1),tick=F,line=-1)
-mtext(side=1,at=0.5,"Intra-class correlation coefficient (ICC)",line=1)
+axis(side=1,seq(0,1,0.1),tick=F,line=-1,cex.axis=0.6)
+mtext(side=1,at=0.5,"Intra-class correlation coefficient (ICC)",line=1,cex=0.6)
 segments(y0=rep(1,10),y1=rep(63,10),x0=seq(0,1,0.1),x1=seq(0,1,0.1),col="grey")
 segments(x0=rep(0,63),x1=rep(1,63),y0=seq(1,62,1)-1/2,y1=seq(1,62,1)-1/2,col="grey")
 polygon(x=c(0,1,1,0,0),y=c(0,0,63,63,0),lwd=2)
@@ -330,13 +369,13 @@ points(seq(1,62,1),x=ICC[srtd,2],pch=22,bg=icccols[2])
 points(seq(1,62,1),x=ICC[srtd,3],pch=21,bg=icccols[3])
 points(seq(1,62,1),x=ICC[srtd,4],pch=24,bg=icccols[4])
 par(xpd=T)
-text(x=rep(-0.05,62),y=seq(1,62,1),namelist[[3]][srtd],adj=1,cex=0.6)
+text(x=rep(-0.05,62),y=seq(1,62,1),rownames(ICC)[srtd],adj=1,cex=0.6)
 
 legend(x=0.58,y=8.5,c("-2 hr","4 hr","8 hr","12 hr"),pt.bg=icccols,pch=c(1,22,21,24),
        ncol=2,bg="white")
 
 
-#dev.off()
+dev.off()
 
 #-----------------------------------------------------------------------------------------------------------
 # TO BE UPDATED
@@ -349,7 +388,7 @@ par(mar=c(5,13.5,4,2)+0.1)
 barplot(t(as.matrix(VC/rowSums(VC))[order((VC/rowSums(VC))[,1]),]),beside=F,horiz=T,xaxt="n",las=1,col=barcols) # relative proportions
 legend(x=0.45,y=8,c("between","within"),horiz=T,pch=c(15,15),col=barcols,bg="white")
 axis(side=1,at=seq(0,1,0.2),labels=paste0(seq(0,100,20),"%"))
-dev.off()
+#dev.off()
 
 
 #-----------------------------------------------------------------------------------------------------------
